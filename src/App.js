57c1ec386e3799157e0cf29cf0be1e300cad7f22
NatalaBoiko <92225403+NatalaBoiko@users.noreply.github.com>
2023-03-20 21:14:30 +0200

    suspense

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 
 import "./App.css";
 
-// import Home from "./pages/Home/Home";
-import Character from "./pages/Character/Character";
-import NotFound from "./pages/NotFound/NotFound";
-
 const Home = lazy(() => import("./pages/Home/Home"));
-// const NotFound = lazy(() => import("./pages/NotFound/NotFound"));
+const Character = lazy(() => import("./pages/Character/Character"));
+
+const NotFound = lazy(() => import("./pages/NotFound/NotFound"));
 
 function App() {
   return (
     <div className="app">
       {/* <Menu /> */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:id" element={<Character />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:id" element={<Character />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
